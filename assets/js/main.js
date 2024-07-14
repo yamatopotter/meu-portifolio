ec2d9b642b6050407e5afdb604c168861f4a8044
Matheus Barreto <matheuspbarreto@hotmail.com.br>
2024-07-14 20:02:42 -0300

    add: Menu mobile on tablet

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+function openMenu() {
+  let menu = document.getElementById("menu");
+  menu.classList.add("show-mobile");
+}
+
+function closeMenu() {
+  let menu = document.getElementById("menu");
+  menu.classList.remove("show-mobile");
+}
